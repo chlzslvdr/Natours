93e0dcbcfee8fa52e2472326b00e886aa22eb5db
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-12 20:30:24 +0800

    Handling POST Requests

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+// Middleware =  is basically a function that can modify the incoming request data.
+app.use(express.json());
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -16,6 +19,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours: tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
