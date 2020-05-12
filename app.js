fdb90a0eb192540ae7109ff02b0bdf696d5beac1
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-12 19:01:09 +0800

    Handling GET Requests

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+const fs = require('fs');
 const express = require('express');
 const port = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
 });
 
 app.listen(port, () => {
