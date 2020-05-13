f30ffd67afeb37d3e023bf1cf99d9cb1b5c505cd
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-13 09:59:05 +0800

    Handling DELETE Requests

--- app.js
+++ app.js
@@ -77,6 +77,20 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 app.listen(port, () => {
   console.log(`🚀 App running on port ${port}...`);
 });
