68780a526388b12a6112cef7fd03dd08ebcae137
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-13 12:24:39 +0800

    Serving Static Files

--- app.js
+++ app.js
@@ -9,16 +9,7 @@ const app = express();
 // MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log('Hello from middleware ✋');
-  next();
-});
-
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
