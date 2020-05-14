15f9c9aa158858ccab66668b621852e5493f8135
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-14 12:57:22 +0800

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -38,6 +38,20 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR 💥', err);
+  });
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`🚀 App running on port ${port}...`);
