3b76f0f137562e8673c49d1d4d1fcdd70c111e58
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-17 18:15:14 +0800

    Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,6 +30,19 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 5. Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10, 1-10 page 1, 11-20 page 2
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
