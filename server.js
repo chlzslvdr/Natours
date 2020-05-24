646bd7fd87eae2a3999e28ddb0a9d88083d80645
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-24 14:08:40 +0800

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -20,7 +26,7 @@ mongoose
     console.log('DB connection successful! 👋');
   });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`🚀 App running on port ${port}...`);
 });
