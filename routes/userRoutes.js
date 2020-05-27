fd2639a7f2a37bbb0aa25bd3a63f24daba36e16a
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-27 10:47:04 +0800

    Updating the Current User: Password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -18,6 +18,12 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+router.patch(
+  '/updateMyPassword',
+  authController.protect,
+  authController.updatePassword
+);
+
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
