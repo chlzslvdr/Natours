b17aebf69d5e53cce0cf5a8c79c19e3b925e6d60
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-31 10:20:58 +0800

    Populating Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -15,12 +15,12 @@ const reviewSchema = new mongoose.Schema(
       type: Date,
       default: Date.now(),
     },
-    tours: {
+    tour: {
       type: mongoose.Schema.ObjectId,
       ref: 'Tour',
       required: [true, 'Review must belong to a tour.'],
     },
-    users: {
+    user: {
       type: mongoose.Schema.ObjectId,
       ref: 'User',
       required: [true, 'Review must belong to a user.'],
@@ -31,6 +31,18 @@ const reviewSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+
+  next();
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
