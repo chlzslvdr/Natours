08d7d38fd5df62922bce1bf2e710725d36455b17
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-02-24 13:58:29 +0800

    Logging out Users

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,6 +68,15 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'logged out', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+
+  res.status(200).json({ status: 'success' });
+};
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1. Getting token and check if its there
   let token;
