5b35421c4101a6b4b93b574c007a1360e561b8f4
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-03-01 14:22:39 +0800

    Saving Image Name to Database

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
     console.info('DB connection successful! 👋');
   });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.info(`🚀 App running on http://127.0.0.1:${port}`);
 });
