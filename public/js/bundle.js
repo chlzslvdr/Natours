42b19c7493c5e2b151b03d639550f7d08db629db
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-03-01 15:07:43 +0800

    Adding Image Uploads to Form

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9306,12 +9306,11 @@ if (loginForm) loginForm.addEventListener('submit', function (event) {
 if (logOutBtn) logOutBtn.addEventListener('click', _login.logout);
 if (userDataForm) userDataForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, 'data');
+  var form = new FormData();
+  form.append('name', document.getElementById('name').value);
+  form.append('email', document.getElementById('email').value);
+  form.append('photo', document.getElementById('photo').files[0]);
+  (0, _updateSettings.updateSettings)(form, 'data');
 });
 if (userPasswordForm) userPasswordForm.addEventListener('submit', /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(event) {
@@ -9378,7 +9377,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55381" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55482" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
