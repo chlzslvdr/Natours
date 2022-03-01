bf023ea39b5715ecc87ff3f681c1c82e33674105
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-03-01 14:12:02 +0800

    Configuring Multer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,8 @@
 const express = require('express');
-const multer = require('multer');
-const userControllers = require('./../controllers/userController');
+const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
-const upload = multer({ dest: 'public/img/users' });
-
 const router = express.Router();
-
-const {
-  getAllUsers,
-  createUser,
-  getUser,
-  updateUser,
-  deleteUser,
-  deleteMe,
-  updateMe,
-  getMe,
-} = userControllers;
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
@@ -30,13 +16,24 @@ router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
 
-router.get('/me', getMe, getUser);
-router.patch('/updateMe', upload.single('photo'), updateMe);
-router.delete('/deleteMe', deleteMe);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.updateMe
+);
+router.delete('/deleteMe', userController.deleteMe);
 
 router.use(authController.restrictTo('admin'));
 
-router.route('/').get(getAllUsers).post(createUser);
-router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
