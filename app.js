99e953ce95a2ae5d2f89a1acd021cf847fde59b1
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-03-02 10:39:04 +0800

    Processing Multiple Images

--- app.js
+++ app.js
@@ -35,7 +35,31 @@ app.use(
 );
 
 // Set security HTTP headers
-app.use(helmet());
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'", 'data:', 'blob:'],
+      baseUri: ["'self'"],
+      fontSrc: ["'self'", 'https:', 'data:'],
+      scriptSrc: ["'self'", 'https://*.cloudflare.com'],
+      scriptSrc: ["'self'", 'https://*.stripe.com'],
+      scriptSrc: ["'self'", 'http:', 'https://*.mapbox.com', 'data:'],
+      frameSrc: ["'self'", 'https://*.stripe.com'],
+      objectSrc: ["'none'"],
+      styleSrc: ["'self'", 'https:', 'unsafe-inline'],
+      workerSrc: ["'self'", 'data:', 'blob:'],
+      childSrc: ["'self'", 'blob:'],
+      imgSrc: ["'self'", 'data:', 'blob:'],
+      connectSrc: [
+        "'self'",
+        'blob:',
+        'https://*.mapbox.com',
+        'https://*.cloudflare.com',
+      ],
+      upgradeInsecureRequests: [],
+    },
+  })
+);
 
 // Development logging
 if (process.env.NODE_ENV === 'development') {
