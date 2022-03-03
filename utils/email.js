58c2955c299e48705420bfe16c3eae829dacaa88
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-03-03 09:30:01 +0800

    Sending Password Reset Emails

--- utils/email.js
+++ utils/email.js
@@ -52,4 +52,11 @@ module.exports = class Email {
   async sendWelcome() {
     await this.send('welcome', 'Welcome to the Natours Family!');
   }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token is valid for 10 minutes'
+    );
+  }
 };
