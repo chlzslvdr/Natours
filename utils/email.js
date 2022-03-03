8af126fc67bb97922de222ad21c8bc7525f555bf
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-03-03 15:28:27 +0800

    Using Sendgrid for Real Emails

--- utils/email.js
+++ utils/email.js
@@ -7,13 +7,21 @@ module.exports = class Email {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
-    this.from = `Jakob Rolfson <${process.env.EMAIL_FROM}>`;
+    this.from =
+      process.env.NODE_ENV === 'production'
+        ? `Jakob Rolfson <${process.env.EMAIL_FROM_PROD}>`
+        : `Jakob Rolfson <${process.env.EMAIL_FROM}>`;
   }
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      // Sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
     }
 
     return nodemailer.createTransport({
