274b4e6a07bc74222e3924f3936ad8f47c41dbec
Chelzea Salvador <chelzeasalvador@gmail.com>
2022-08-27 13:57:00 +0800

    fix deploy error

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 
-import '@babel/polyfill';
+import 'core-js/es7/reflect';
 import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
